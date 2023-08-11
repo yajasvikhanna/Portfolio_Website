6080788a03ad323d0296980e27c2ddccd51cb650
Yajasvi <yajasvikhanna@gmail.com>
2023-08-11 18:10:21 +0530

    Created About Page and footer

--- script.js
+++ script.js
@@ -1,32 +1,115 @@
-// function loco(){
-//     gsap.registerPlugin(ScrollTrigger);
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
-// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+var Timeout;
+function firstPageAnim(){
+    var tl = gsap.timeline();
 
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector("#main"),
-//   smooth: true
-// });
-// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-// locoScroll.on("scroll", ScrollTrigger.update);
+    tl.from("#nav", {
+        y:'-10',
+        opacity: 0,
+        duration: 2,
+        ease:Expo.easeInOut
+    })
+    .to(".boundingelem", {
+        y:0,
+        duration:2,
+        ease: Expo.easeInOut,
+        // staggger:.2,
+        delay: -1
+    })
+    .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    
+    })
 
-// // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-// ScrollTrigger.scrollerProxy("#main", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-//   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-// });
 
-// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+}
 
-// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-// ScrollTrigger.refresh();
+function flatcircle()
+{   // define default scale value
+    var xscale=1;
+    var yscale=1;
 
-// }
-// loco()
+    var xprev=0;
+    var yprev=0;
+
+    window.addEventListener("mousemove", function(dets){
+        clearTimeout(Timeout);
+        xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev);
+
+       xprev= dets.clientX;
+       yprev= dets.clientY;
+
+
+      circleMouseFollower(xscale, yscale);
+      Timeout= setTimeout(function() {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
+      }, 100);
+
+    });
+}
+
+function circleMouseFollower(xscale,yscale){
+    window.addEventListener("mousemove", function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+    })
+}
+
+flatcircle(); 
+circleMouseFollower();
+firstPageAnim();
+
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
+  });
+
+
+  function updateClock() {
+    const currentDate = new Date();
+
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    const date = document.getElementById("date");
+     date.innerText=currentDate;
+
+}
+
+// Update the clock initially
+updateClock();
+
+// Update the clock every second (1000 milliseconds)
+setInterval(updateClock, 1000);
+
+
